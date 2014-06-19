68312724ba1d219aae7b2f90e4fcaf31efc15e08
David Hernández Bethencourt <alu0100536881@ull.edu.es>
2014-06-19 19:37:39 +0100

    Modificar el Game Stack Engine, autoajustar el tamaño del canvas en función de la pantalla y definir bien los event listeners

--- js/puzzleyap.js
+++ js/puzzleyap.js
@@ -2,7 +2,8 @@
 /*global _*/
 
 (function () {
-  "use strict";
+
+  'use strict';
   
   var isFunction = function (possibleFunction) {
     return (typeof (possibleFunction) === typeof (Function));
@@ -11,118 +12,268 @@
   // Espacio de nombres (namespace) del juego
   var PUZZLEYAP = {
 
+    /* Available mobile device: Samsung Galaxy Mini 2
+       Screen: 320x480 */
+
     // Tamaño del canvas
-    WIDTH: 320,  // Ancho
-    HEIGHT: 480,  // Alto
+    WIDTH: navigator.isCocoonJS ? window.innerWidth : 320, // Ancho
+    HEIGHT: navigator.isCocoonJS ? window.innerHeight : 480,  // Alto
+    dips: window.devicePixelRatio,
+    offset: {top: 0, left: 0},
+    fontBase: 480,
 
     // El canvas y su contexto
     canvas: null,
     ctx: null,
+    gameMode: null,
+
+    // Inicializa el canvas
+    setCanvas: function () {
+      console.log("Device pixel ratio: " + this.dips);
+      console.log("Resolución: " + this.WIDTH + "x" + this.HEIGHT);
+      console.log("Device pixel ratio: " + this.dips);
+      
+      this.canvas = document.createElement("canvas");
+      this.canvas.width = this.WIDTH * this.dips;
+      this.canvas.height = this.HEIGHT * this.dips;
+      this.canvas.id = "gamePuzzleYap";
+
+      this.ctx = this.canvas.getContext("2d");
+      
+      PUZZLEYAP.Draw.rect(0, 0, this.canvas.width, this.canvas.height, "#33ff89");
+      document.body.appendChild(this.canvas);
+      
+
+      this.offset.top = this.canvas.offsetTop;
+      this.offset.left = this.canvas.offsetLeft;
+    },
+
+    // Manejadores de evento para click/touch
+    setTouchAndMouseEventListeners: function () {
+      if (window.PointerEvent) {
+        console.log("El navegador soporta Pointer Events");
+
+        this.canvas.addEventListener("pointerdown", function (e) {
+          e.preventDefault();
+          PUZZLEYAP.Input.setTapped(e);
+        }, false);
+
+        this.canvas.addEventListener("pointerup", function (e) {
+          e.preventDefault();
+          PUZZLEYAP.Input.unsetTapped();
+        }, false);
+
+        this.canvas.addEventListener("pointermove", function (e) {
+          e.preventDefault();
+          PUZZLEYAP.Input.setHovered(e);
+        });
+
+        if (navigator.maxTouchPoints > 1) {
+          /* Tanto el navegador como el hardware soportan toques multiples
+             Tener en cuenta de cara a expandir funcionalidad en el futuro */
+          console.log("Soporte para toques múltiples");
+        }
+      } else {
+        if (navigator.isCocoonJS) {
+
+          console.log("El navegador está en CocoonJS, osease, Touch Events");
+
+          // Eventos de tocar (touch) para móviles
+          this.canvas.addEventListener('touchstart', function (e) {
+            e.preventDefault();
+            // El objeto de evento tiene un array llamado
+            // touches. Solo se requiere el primer toque,
+            // el cual pasaremos como input
+            PUZZLEYAP.Input.setTapped(e.touches[0]);
+          }, false);
+
+          this.canvas.addEventListener('touchmove', function (e) {
+            // No es necesario de momento pero por si acaso
+            // prevenimos su comportamiento por defecto
+            e.preventDefault();
+          }, false);
+
+          this.canvas.addEventListener('touchend', function (e) {
+            e.preventDefault();
+            // Reiniciamos las propiedades del input una vez
+            // finalizado el toque
+            PUZZLEYAP.Input.reset();
+          }, false);
+
+        } else {
+          console.log("El navegador será web, osease, Mouse Events");
+
+          this.canvas.addEventListener("mousedown", function (e) {
+            e.preventDefault();
+            PUZZLEYAP.Input.setTapped(e);
+            console.log(PUZZLEYAP.Input);
+          }, false);
+
+          this.canvas.addEventListener("mouseup", function (e) {
+            e.preventDefault();
+            PUZZLEYAP.Input.unsetTapped();
+            console.log(PUZZLEYAP.Input);
+          }, false);
+
+          this.canvas.addEventListener("mousemove", function (e) {
+            e.preventDefault();
+            PUZZLEYAP.Input.setHovered(e);
+          });
+        }
+      }
+
+    },
 
     init: function () {
-      PUZZLEYAP.canvas = document.createElement("canvas");
-      PUZZLEYAP.canvas.width = PUZZLEYAP.WIDTH;
-      PUZZLEYAP.canvas.height = PUZZLEYAP.HEIGHT;
-      PUZZLEYAP.canvas.id = "gamePuzzleYap";
+      this.setCanvas();
+      this.setTouchAndMouseEventListeners();
 
-      PUZZLEYAP.ctx = PUZZLEYAP.canvas.getContext("2d");
-      PUZZLEYAP.ctx.fillStyle = "#33ff89";
-      PUZZLEYAP.ctx.fillRect(0, 0, PUZZLEYAP.canvas.width, PUZZLEYAP.canvas.height);
-      document.body.appendChild(PUZZLEYAP.canvas);
-      
-      // Entidades u Objetos del juego
-      PUZZLEYAP.gameObjects = [];
-
-      // listen for clicks
-      window.addEventListener('click', function (e) {
-        e.preventDefault();
-        PUZZLEYAP.Input.set(e);
-      }, false);
-
-      // Eventos de tocar (touch) para móviles
-      window.addEventListener('touchstart', function (e) {
-        e.preventDefault();
-        // El objeto de evento tiene un array llamado
-        // touches. Solo se requiere el primer toque,
-        // el cual pasaremos como input
-        PUZZLEYAP.Input.set(e.touches[0]);
-      }, false);
-
-      window.addEventListener('touchmove', function (e) {
-        // No es necesario de momento pero por si acaso
-        // prevenimos su comportamiento por defecto
-        e.preventDefault();
-      }, false);
-
-      window.addEventListener('touchend', function (e) {
-        e.preventDefault();
-        // Reiniciamos las propiedades del input una vez
-        // finalizado el toque
-        PUZZLEYAP.Input.unset();
-      }, false);
-      
-      var gameMenu = new PUZZLEYAP.Menu("P", 80, 80);
-
-      PUZZLEYAP.gameObjects.push(gameMenu);
-      
+      this.gameMode = new this.StateStack();
+      this.gameMode.push(new this.MainMenuState());
       
       // Comienza a ejecutar el bucle del juego
-      PUZZLEYAP.loop();
+      PUZZLEYAP.gameLoop();
     },
 
     // Actualiza el estado la pantalla
     update: function () {
-      if (PUZZLEYAP.Input.tapped) {
-        _.each(PUZZLEYAP.gameObjects, function (obj) {
-          obj.update(PUZZLEYAP.Input);
-          PUZZLEYAP.Input.tapped = false;
-        });
-      } else {
-        _.each(PUZZLEYAP.gameObjects, function (obj) {
-          obj.update();
-        });
-      }
-      
+      this.gameMode.update();
     },
 
     // Visualiza los cambios
     render: function () {
-      PUZZLEYAP.Draw.text("PuzzleYap", 40, 40, 40, "Black");
-      _.each(PUZZLEYAP.gameObjects, function (obj) {
-        obj.draw();
-      });
+      this.gameMode.render();
     },
 
     // Bucle del juego
-    loop: function () {
-      window.requestAnimFrame(PUZZLEYAP.loop);
+    gameLoop: function () {
+      window.requestAnimFrame(PUZZLEYAP.gameLoop);
       
       PUZZLEYAP.update();
       PUZZLEYAP.render();
+    },
+
+    EmptyState: function () {
+      this.name = ""; // Just to identify the State
+      this.update = function () {};
+      this.render = function () {};
+      this.onEnter = function () {};
+      this.onExit = function () {};
+
+      // Optional but useful
+      this.onPause = function () {};
+      this.onResume = function () {};
+    },
+
+    StateList: function () {
+      var states = [];
+      this.pop = function () {
+        return states.pop();
+      };
+      this.push = function (state) {
+        states.push(state);
+      };
+      this.top = function () {
+        return states[states.length - 1];
+      };
+    },
+
+    StateStack: function () {
+      var states = new PUZZLEYAP.StateList();
+      states.push(new PUZZLEYAP.EmptyState());
+
+      this.update = function () {
+        var state = states.top();
+        if (state) {
+          state.update();
+        }
+      };
+
+      this.render = function () {
+        var state = states.top();
+        if (state) {
+          state.render();
+        }
+      };
+
+      this.push = function (state) {
+        states.push(state);
+        state.onEnter();
+      };
+
+      this.pop = function () {
+        var state = states.top();
+        state.onExit();
+        return states.pop();
+      };
+
+      this.pause = function () {
+        var state = states.top();
+        if (state.onPause) {
+          state.onPause();
+        }
+      };
+
+      this.resume = function () {
+        var state = states.top();
+        if (state.onResume) {
+          state.onResume();
+        }
+      };
+    },
+
+    pauseGame: function () {
+      clearInterval(this.gameLoop);
+    },
+
+    resumeGame: function () {
+      this.gameLoop();
     }
 
   };
 
-  // Objeto de entrada (input) para manejar los toques
+
+
+  // Objeto de entrada (input) para manejar los toques/clicks
   PUZZLEYAP.Input = {
 
     x: 0,
     y: 0,
     tapped: false,
+    hovered: false,
 
-    set: function (data) {
-      this.x = data.pageX;
-      this.y = data.pageY;
+    setTapped: function (data) {
+      this.x = data.pageX - PUZZLEYAP.offset.left;
+      this.y = data.pageY - PUZZLEYAP.offset.top;
       this.tapped = true;
-
+      console.log("[" + this.x + ", " + this.y + "]");
     },
 
-    unset: function () {
+    setHovered: function (data) {
+      this.x = data.pageX - PUZZLEYAP.offset.left;
+      this.y = data.pageY - PUZZLEYAP.offset.top;
+      this.hovered = true;
+    },
+
+    unsetTapped: function () {
+      this.tapped = false;
+    },
+
+    unsetHovered: function () {
+      this.hovered = false;
+    },
+
+    reset: function () {
       this.x = 0;
       this.y = 0;
+      this.hovered = false;
       this.tapped = false;
     }
+  };
 
+  PUZZLEYAP.getProperFont = function (fontSize) {
+    var ratio = fontSize / PUZZLEYAP.fontBase;
+    return Math.round(PUZZLEYAP.WIDTH * ratio);
   };
 
   // Abstraer varias operaciones de canvas en funciones standalone
@@ -130,11 +281,16 @@
 
     clear: function () {
       PUZZLEYAP.ctx.clearRect(0, 0, PUZZLEYAP.WIDTH, PUZZLEYAP.HEIGHT);
+      PUZZLEYAP.Draw.rect(0, 0, PUZZLEYAP.WIDTH, PUZZLEYAP.HEIGHT, "#33ff89");
     },
 
-    rect: function (x, y, width, height, color) {
+    rect: function (x, y, width, height, color, border) {
       PUZZLEYAP.ctx.fillStyle = color;
       PUZZLEYAP.ctx.fillRect(x, y, width, height);
+      if (border) {
+        PUZZLEYAP.ctx.rect(x, y, width, height);
+        PUZZLEYAP.ctx.stroke();
+      }
     },
 
     text: function (string, x, y, size, color) {
@@ -155,10 +311,16 @@
       return (touch.x >= object.x) && (touch.x <= object.x + object.width) &&
         (touch.y >= object.y) && (touch.y <= object.y + object.height);
     },
-    updateStats: function (input) {
-      if (this.intersects(this, input) && input.tapped) {
-        this.touched = true;
+    updateStats: function () {
+      if (this.intersects(this, PUZZLEYAP.Input)) {
+        this.hovered = true;
+        if (PUZZLEYAP.Input.tapped) {
+          this.touched = true;
+        } else {
+          this.touched = false;
+        }
       } else {
+        this.hovered = false;
         this.touched = false;
       }
     }
@@ -171,88 +333,116 @@
     this.width = width;
     this.height = height;
     this.touched = false;
+    this.hovered = false;
     this.text = text;
   };
 
   PUZZLEYAP.UIObject.Button.prototype = _.extend(PUZZLEYAP.UIObject.Button.prototype,
                                                  PUZZLEYAP.UIObject);
   
-  PUZZLEYAP.UIObject.Button.prototype.update = function (input) {
+  PUZZLEYAP.UIObject.Button.prototype.update = function () {
     var wasNotTouched = !this.touched;
-    this.updateStats(input);
+    this.updateStats();
 
     if (this.touched && wasNotTouched) {
       if (!_.isUndefined(this.handler)) {
         this.handler();
+        this.touched = false;
       }
     }
   };
   
+  PUZZLEYAP.UIObject.Button.prototype.unsetHandler = function () {
+    this.handler = null;
+  };
+
   PUZZLEYAP.UIObject.Button.prototype.draw = function () {
-    //draw button
-    PUZZLEYAP.Draw.rect(this.x, this.y, this.width, this.height, "yellow");
+    if (this.hovered) {
+      PUZZLEYAP.Draw.rect(this.x, this.y, this.width, this.height, "#e2e258", true);
+    } else {
+      PUZZLEYAP.Draw.rect(this.x, this.y, this.width, this.height, "yellow", true);
+    }
 
     //text options
-    var fontSize = 20,
+    var fontSize = PUZZLEYAP.getProperFont(22),
+      textX,
+      textY,
+      textSize;
 
     //text position
-      textSize = PUZZLEYAP.ctx.measureText(this.text),
-      textX = this.x + (this.width / 2) - (textSize.width / 2),
-      textY = this.y + (this.height / 2) + (fontSize / 4);
+    PUZZLEYAP.ctx.font = "bold " + fontSize + "px Monospace";
+    textSize = PUZZLEYAP.ctx.measureText(this.text).width / 2;
+    textX = this.x + (this.width / 2) - textSize;
+    textY = this.y + (this.height / 2) + fontSize / 4;
 
     //draw the text
     PUZZLEYAP.Draw.text(this.text, textX, textY, fontSize, "black");
   };
   
   // Pantalla de menú principal
-  PUZZLEYAP.Menu = function (title, x, y) {
+  PUZZLEYAP.MainMenuState = function () {
     this.menuElements = [];
-    this.showMainMenu = true;
-    this.setShowMainMenu = function (allowShow) {
-      this.showMainMenu = allowShow;
-    };
+    this.name = "PuzzleYap";
     
-    // MENU OBJECTS
-    //var titleMenuElement;
-    var playMenuButton = new PUZZLEYAP.UIObject.Button("Jugar", x, y + 20, 160, 40);
-    var aboutMenuButton = new PUZZLEYAP.UIObject.Button("Acerca de", x, y + 90, 160, 40);
-    var exitMenuButton = new PUZZLEYAP.UIObject.Button("Salir", x, y + 160, 160, 40);
-    
-    // MENU HANDLERS
-    playMenuButton.handler = function () {
-      PUZZLEYAP.Draw.text("Hello World 1", 40, 340, 20, "blue");
-    };
-    
-    aboutMenuButton.handler = function () {
-      PUZZLEYAP.Draw.text("Hello World 2", 40, 380, 20, "blue");
-    };
-    
-    exitMenuButton.handler = function () {
-      PUZZLEYAP.Draw.text("Hello World 3", 40, 420, 20, "blue");
-    };
-    
-    this.menuElements.push(playMenuButton);
-    this.menuElements.push(aboutMenuButton);
-    this.menuElements.push(exitMenuButton);
+    this.onEnter = function () {
+      var buttonWidth = PUZZLEYAP.WIDTH / 2 + PUZZLEYAP.WIDTH / 8,
+        buttonHeight = PUZZLEYAP.HEIGHT / 12,
+        buttonX = (PUZZLEYAP.WIDTH - buttonWidth) / 2,
+        thirdHeight = PUZZLEYAP.HEIGHT / 3,
+        buttonY = thirdHeight / 2,
+        fontSize = PUZZLEYAP.getProperFont(75),
+        textWidth,
+        textX,
+        playMenuButton,
+        exitMenuButton;
 
-  };
-  
-  PUZZLEYAP.Menu.prototype.update = function (input) {
-    _.each(this.menuElements, function (element) {
-      if (isFunction(element.update)) {
-        element.update(input);
-      }
-    });
-  };
-  
-  PUZZLEYAP.Menu.prototype.draw = function () {
-    if (this.showMainMenu) {
+      PUZZLEYAP.ctx.font = "bold " + fontSize + "px Monospace";
+      textWidth = PUZZLEYAP.ctx.measureText(this.name).width / 2;
+      textX = (PUZZLEYAP.WIDTH / 2) - textWidth;
+      PUZZLEYAP.Draw.text(this.name, textX, buttonY + 30, fontSize, "black");
+
+      // MENU BUTTOMS
+      playMenuButton = new PUZZLEYAP.UIObject.Button("Renderizar imagen", buttonX,
+        buttonY + thirdHeight - buttonHeight / 2, buttonWidth, buttonHeight);
+      exitMenuButton = new PUZZLEYAP.UIObject.Button("Salir", buttonX,
+        buttonY + 2 * thirdHeight - buttonHeight / 2, buttonWidth, buttonHeight);
+
+      // MENU HANDLERS
+      playMenuButton.handler = function () {
+        // Nota: al crear un dialogo con alert no se llega a realizar el
+        // event listener de mouseup
+        PUZZLEYAP.gameMode.pop();
+        PUZZLEYAP.gameMode.push(new RenderImageState());
+      };
+
+      exitMenuButton.handler = function () {
+        PUZZLEYAP.Draw.text("HOLA MUNDO", textX, 500, 60, "blue");
+      };
+
+      this.menuElements.push(playMenuButton);
+      this.menuElements.push(exitMenuButton);
+
+    };
+    
+    this.onExit = function () {
+      _.each(this.menuElements, function (element) {
+        element.unsetHandler();
+      });
+      PUZZLEYAP.Draw.clear();
+    };
+    
+    this.update = function () {
+      _.each(this.menuElements, function (element) {
+        element.update();
+      });
+    };
+    
+    this.render = function () {
       _.each(this.menuElements, function (element) {
         element.draw();
       });
-    } else {
-      //render about section
-    }
+    };
+
   };
   
   // Inicializa el juego cuando esté todo listo
